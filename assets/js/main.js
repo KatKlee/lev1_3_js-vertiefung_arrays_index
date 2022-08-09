d4cd036a78de2446c162db64505a3770c43f898f
KatKlee <katklee2022@gmail.com>
2022-08-09 13:23:07 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const numberArray = [5, 6, 7, 8, 9, 10]
+console.log(numberArray)
+
+console.log(numberArray[4]) // Zahl 9
+
+console.log(numberArray[0]) // Zahl 5
+console.log(numberArray[1]) // Zahl 6
+console.log(numberArray[2]) // Zahl 7
+console.log(numberArray[3]) // Zahl 8
+console.log(numberArray[5]) // Zahl 10
